38a7bf83f273896eb83251283882227b54f65ec2
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2023-02-06 12:30:51 +0100

    (test) Crear tests de la clase Ask

--- src/test/java/edu/craptocraft/itemTest/AskTest.java
+++ src/test/java/edu/craptocraft/itemTest/AskTest.java
@@ -1,4 +1,31 @@
 package edu.craptocraft.itemTest;
 
+import edu.craptocraft.item.Ask;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class AskTest {
+    Ask ask;
+    @BeforeClass
+    public static void inicializar(){
+        ask = new Ask("37", 900);
+    }
+    @Test
+    public void constructorTest(){
+        Assert.assertEquals("37", ask.size());
+        Assert.assertEquals(900, ask.value());
+    }
+    @Test
+    public void compareToTest(){
+        Ask askBaja = new Ask("55", 450);
+        Assert.assertEquals(0, ask.compareTo(askBaja));
+    }
+    @Test
+    public void toStringTest(){
+        String actual = ask.toString();
+        String esperado = "\t\t37\t900\n";
+        Assert.assertEquals(esperado, actual);
+    }
+
 }
