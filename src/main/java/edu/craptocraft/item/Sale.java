f283cecce441ea7601d273471a341a3a3bc20466
isaacvt01 <_vasqueztucent@cifpfbmoll.eu>
2023-02-07 11:38:23 +0100

    Añadir test y métodos a la clase Sale

--- src/main/java/edu/craptocraft/item/Sale.java
+++ src/main/java/edu/craptocraft/item/Sale.java
@@ -1,4 +1,30 @@
 package edu.craptocraft.item;
 
-public class Sale {
+public class Sale implements Offer {
+
+    private String size;
+    private Integer price;
+
+    public Sale(String size, Integer ask) {
+        this.size = size;
+        this.price = ask;
+    }
+    @Override
+    public String size() {
+        return this.size;
+    }
+    @Override
+    public int value() {
+        return this.price.intValue();
+    }
+    @Override
+    public int compareTo(Offer offer) {
+        return this.price.compareTo(offer.value());
+    }
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\t\t").append(this.size).append("\t").append(this.price).append("\n");
+        return sb.toString();
+    }
 }
--- src/test/java/edu/craptocraft/itemTest/SaleTest.java
+++ src/test/java/edu/craptocraft/itemTest/SaleTest.java
@@ -1,4 +1,36 @@
 package edu.craptocraft.itemTest;
 
+import edu.craptocraft.item.Ask;
+import edu.craptocraft.item.Bid;
+import edu.craptocraft.item.Sale;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class SaleTest {
+    static Sale sale;
+    @BeforeClass
+    public static void inicializar(){
+        sale = new Sale("37", 900);
+    }
+    @Test
+    public void constructorTest(){
+        Assert.assertEquals("37", sale.size());
+        Assert.assertEquals(900, sale.value());
+    }
+    @Test
+    public void compareToTest(){
+        Sale saleBaja = new Sale("55", 450);
+        Sale saleAlta = new Sale("33", 1500);
+        Sale saleIgual = new Sale("44", 900);
+        Assert.assertEquals(1, sale.compareTo(saleBaja));
+        Assert.assertEquals(-1, sale.compareTo(saleAlta));
+        Assert.assertEquals(0, sale.compareTo(saleIgual));
+    }
+    @Test
+    public void toStringTest(){
+        String actual = sale.toString();
+        String esperado = "\t\t37\t900\n";
+        Assert.assertEquals(esperado, actual);
+    }
 }
